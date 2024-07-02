ea5f858588314d685bd96c185effde41673a6367
Kousha Yeganeh <koushayeganeh@gmail.com>
2024-07-03 00:09:59 +0300

    display notes

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Conneted: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      ssl: true, // Assuming SSL is required
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,14 +1,51 @@
+const Note = require("../models/Note");
+const mongoose = require("mongoose");
+
 // get /
 // dashboard
 
 exports.dashboard = async (req, res) => {
+  let perPage = 8;
+  let page = req.query.page || 1;
+
+  const userId = req.user.id;
+  const objectId = new mongoose.Types.ObjectId(userId);
+
   const locals = {
     title: "Dashboard",
     description: "Easy as NodeJS!",
   };
 
-  res.render("dashboard/index", {
-    locals,
-    layout: "../views/layouts/dashboard",
-  });
+  try {
+    const notes = await Note.aggregate([
+      {
+        $sort: {
+          updatedAt: -1,
+        },
+      },
+      { $match: { user: objectId } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 35] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Note.countDocuments();
+
+    res.render("dashboard/index", {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      currentPage: page,
+      totalPages: Math.ceil(count / perPage),
+      layout: "../views/layouts/dashboard",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
